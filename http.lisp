47cb4e3c24e122a180259ea3b558132eca9ff217
Florian MARGAINE <florian@margaine.com>
2015-02-22 12:55:39 +0100

    Uses parse-trees instead of string regexes

--- http.lisp
+++ http.lisp
@@ -3,15 +3,45 @@
 
 (defvar *request-line-scanner*
   (cl-ppcre:create-scanner
-   (cat
-    "^(GET|HEAD|POST|PUT|DELETE|TRACE|OPTIONS|CONNECT|PATCH) " ; methods
-    "([a-zA-Z0-9%_\\/-]+) " ; request uri
-    "HTTP\\/1.1$") ; only HTTP/1.1 is supported for now
+   '(:sequence
+     :start-anchor
+     (:register ;; methods
+      (:alternation
+       "GET" "HEAD" "POST" "PUT" "DELETE" "TRACE" "OPTIONS" "CONNECT" "PATCH"))
+     #\Space
+     (:register ;; request URI
+      (:greedy-repetition 1 nil
+       (:char-class (:range #\a #\z)
+                    (:range #\A #\Z)
+                    (:range #\0 #\9)
+                    #\%
+                    #\_
+                    #\/
+                    #\-)))
+     #\Space
+     "HTTP" #\/ "1.1"
+     #\Return
+     :end-anchor)
    :case-insensitive-mode t))
 
 (defvar *request-header-scanner*
   (cl-ppcre:create-scanner
-   "^\\s*([a-zA-Z0-9-]+)\\s*:\\s*(.+)$"))
+   '(:sequence
+     :start-anchor
+     (:greedy-repetition 0 nil :whitespace-char-class)
+     (:register ;; header name
+      (:greedy-repetition 1 nil
+       (:char-class (:range #\a #\z)
+                    (:range #\A #\Z)
+                    (:range #\0 #\9)
+                    #\-)))
+     (:greedy-repetition 0 nil :whitespace-char-class)
+     #\:
+     (:greedy-repetition 0 nil :whitespace-char-class)
+     (:register ;; header value
+      (:greedy-repetition 1 nil :everything))
+     #\Return
+     :end-anchor)))
 
 (defun http-request-parse-lines (lines &optional req)
   (unless lines
