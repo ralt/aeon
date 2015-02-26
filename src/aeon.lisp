beb5172d4fa06c6d7971960a486cd17e733dccf6
Florian MARGAINE <florian@margaine.com>
2015-02-27 00:43:40 +0100

    Adds request line

--- src/aeon.lisp
+++ src/aeon.lisp
@@ -15,7 +15,8 @@
   (q+:hide (q+:horizontal-header headers-list))
   (q+:hide (q+:vertical-header headers-list))
   (setf (q+:stretch-last-section (q+:horizontal-header headers-list)) t)
-  (setf (q+:resize-mode (q+:horizontal-header headers-list)) (q+:qheaderview.resize-to-contents)))
+  (setf (q+:resize-mode (q+:horizontal-header headers-list))
+        (q+:qheaderview.resize-to-contents)))
 
 (define-subwidget (aeon layout) (q+:make-qvboxlayout aeon)
   (setf (q+:window-title aeon) "Aeon")
@@ -36,17 +37,31 @@
 
 (define-slot (aeon got-request) ((request string))
   (declare (connected aeon (got-request string)))
-  (let ((lines (cl-ppcre:split (concat '(#\Return #\Newline)) request)))
-    (setf (q+:column-count headers-list) 2) ;; header / value
+  (let* ((lines (cl-ppcre:split (concat '(#\Newline)) request))
+         (req (http-request-parse-lines lines)))
+    (setf (q+:column-count headers-list) 3)
     (setf (q+:row-count headers-list) (- (length lines) 1))
-    ;; First line is request line, ignore it
+    ;; First line is request line, handle it separately
+    (setf (q+:item headers-list 0 0)
+          (q+:make-qtablewidgetitem (rest (list-get-item 'method req))
+                                    (q+:qtablewidgetitem.type)))
+    (setf (q+:item headers-list 0 1)
+          (q+:make-qtablewidgetitem (rest (list-get-item 'request-uri req))
+                                    (q+:qtablewidgetitem.type)))
+    (setf (q+:item headers-list 0 2)
+          (q+:make-qtablewidgetitem (rest (list-get-item 'version req))
+                                    (q+:qtablewidgetitem.type)))
     (loop for i from 1 upto (- (length lines) 1)
        do (let* ((values (cl-ppcre:split ":" (elt lines i)))
                  (header (elt values 0))
-                 (value (apply #'concat (rest values))))
-            (setf (q+:item headers-list (- i 1) 0)
+                 ; Remove the trailing #\Return
+                 (tmp-value (apply #'concat (rest values)))
+                 (value (subseq tmp-value 0 (- (length tmp-value) 1))))
+            ; spans the 2nd column to 2 columns
+            (setf (q+:span headers-list) (values i 1 1 2))
+            (setf (q+:item headers-list i 0)
                   (q+:make-qtablewidgetitem header (q+:qtablewidgetitem.type)))
-            (setf (q+:item headers-list (- i 1) 1)
+            (setf (q+:item headers-list i 1)
                   (q+:make-qtablewidgetitem value (q+:qtablewidgetitem.type)))))))
 
 (defun main ()
